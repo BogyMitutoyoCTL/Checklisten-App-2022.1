57371006839947451a3a8c081da247dd08104ab9
LFZWladi <rwab657@gmail.com>
2022-04-05 13:02:24 +0200

    First_Check.dart Implementation

---
+++ Source/lib/First_Check.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'Creation.dart';
+
+class FirstCheck extends StatefulWidget {
+  const FirstCheck({Key? key}) : super(key: key);
+
+  @override
+  State<FirstCheck> createState() => _First_CheckState();
+}
+
+class _First_CheckState extends State<FirstCheck> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(title: Text(""), backgroundColor: Colors.white),
+        body: Padding(
+      padding: const EdgeInsets.fromLTRB(
+          100, 350, 50, 50), //ToDO: Größe von Schrift und Button
+      child: ElevatedButton(
+        onPressed: weiter,
+        /*child: Text("Create your first checklist!"),*/ child: Container(height: 80, width: ,)
+        // style: ElevatedButton.styleFrom(primary: Colors.white),
+      ),
+    ));
+  }
+
+  void weiter() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => Creation()));
+  }
+}
