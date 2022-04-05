3ab6f1be98672b2b259de06e9c9bcb788737e84c
Mayer-Lukas <mayerlukas.13.9.2005@gmail.com>
2022-04-05 13:25:17 +0200

    Language Bild im Hintergrund

--- Source/lib/main.dart
+++ Source/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:voodoolist/SplashScreen.dart';
+
+import 'Language.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Splashscreen(),
+      home: Language(),
     );
   }
 }
