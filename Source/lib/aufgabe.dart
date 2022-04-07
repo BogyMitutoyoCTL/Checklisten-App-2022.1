bb7f32885296d7434637572032f2b75f52cb3a61
Mayer-Lukas <mayerlukas.13.9.2005@gmail.com>
2022-04-07 11:34:18 +0200

    checkliste lokal speichern und aufrufen

--- Source/lib/aufgabe.dart
+++ Source/lib/aufgabe.dart
@@ -1,9 +1,45 @@
+import 'dart:convert';
+
+import 'package:voodoolist/checkliste.dart';
+
 class Aufgabe {
   late bool fertig;
   late String Element;
 
-  Aufgabe(bool fertig, String Element) {
-    this.fertig = fertig;
-    this.Element = Element;
+  Aufgabe(this.fertig, this.Element);
+
+  Aufgabe.fromJson(Map<String, dynamic> json)
+      : fertig = json['fertig'],
+        Element = json['Element'];
+
+  Map<String, dynamic> toJson() => {'fertig': fertig, 'Element': Element};
+}
+
+void main() {
+  Checkliste checklist = new Checkliste(
+      "namedercheckliste", [Aufgabe(false, "Element"), Aufgabe(true, "nio")]);
+  String encodedAufgabe = jsonEncode(checklist.toMap());
+  Map<String, dynamic> decodedAufgabe = jsonDecode(encodedAufgabe);
+
+  var key_in_klammern = decodedAufgabe.keys.toString();
+  var checkliste_name =
+      key_in_klammern.substring(1, key_in_klammern.toString().length - 1);
+
+  var checklist_as_map = decodedAufgabe[checkliste_name];
+  List<Aufgabe> aufgaben_liste = [];
+
+  for (String aufgabe_as_map in checklist_as_map) {
+    var fertig = false;
+    var element_from_aufgabe;
+    if (aufgabe_as_map.contains('true')) {
+      fertig = true;
+      element_from_aufgabe =
+          aufgabe_as_map.substring(26, aufgabe_as_map.length - 2);
+    } else {
+      element_from_aufgabe =
+          aufgabe_as_map.substring(27, aufgabe_as_map.length - 2);
+    }
+    aufgaben_liste.add(Aufgabe(fertig, element_from_aufgabe));
   }
+  var c = new Checkliste(checkliste_name, aufgaben_liste);
 }
--- Source/lib/checkliste.dart
+++ Source/lib/checkliste.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'aufgabe.dart';
 
 class Checkliste {
@@ -7,5 +9,13 @@ class Checkliste {
   Checkliste(String titel, List<Aufgabe> aufgaben_liste) {
     this.titel = titel;
     this.aufgaben_liste = aufgaben_liste;
+  }
+
+  Map<String, List<String>> toMap() {
+    List<String> value = [];
+    for (var aufgaben in this.aufgaben_liste) {
+      value.add(jsonEncode(aufgaben));
+    }
+    return {this.titel: value};
   }
 }
