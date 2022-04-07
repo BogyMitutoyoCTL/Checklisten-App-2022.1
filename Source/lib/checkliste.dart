cf6ee9b952b3393a4fdc255fededf9f16189e8e2
Mayer-Lukas <mayerlukas.13.9.2005@gmail.com>
2022-04-07 11:44:11 +0200

    fix #11: lokal speichern funktioniert

--- Source/lib/checkliste.dart
+++ Source/lib/checkliste.dart
@@ -19,3 +19,28 @@ class Checkliste {
     return {this.titel: value};
   }
 }
+
+Checkliste fromMapToChecklist(var checkliste_as_string) {
+  var key_in_klammern = checkliste_as_string.keys.toString();
+  var checkliste_name =
+      key_in_klammern.substring(1, key_in_klammern.length - 1);
+
+  var checklist_as_map = checkliste_as_string[checkliste_name];
+  List<Aufgabe> aufgaben_liste = [];
+
+  for (String aufgabe_as_map in checklist_as_map) {
+    var fertig = false;
+    var element_from_aufgabe;
+    if (aufgabe_as_map.contains('true')) {
+      //TODO: funktioniert nicht richtig, wenn true im text ist
+      fertig = true;
+      element_from_aufgabe =
+          aufgabe_as_map.substring(26, aufgabe_as_map.length - 2);
+    } else {
+      element_from_aufgabe =
+          aufgabe_as_map.substring(27, aufgabe_as_map.length - 2);
+    }
+    aufgaben_liste.add(Aufgabe(fertig, element_from_aufgabe));
+  }
+  return new Checkliste(checkliste_name, aufgaben_liste);
+}
